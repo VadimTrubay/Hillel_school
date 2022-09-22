0def5edf7f33b5ec4f57d66fe730b318ba8d4b98
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-22 20:23:03 +0300

    Initial commit

--- DZ_7/DZ_7_3.py
+++ DZ_7/DZ_7_3.py
@@ -9,6 +9,7 @@ import time
 print('Игра угадай число за три попытки.')
 time.sleep(1)
 hidden_number = random.randint(1, 10)
+print(hidden_number)
 while True:
     question = input('Сыграем? y/n >: \n')
     time.sleep(1)
@@ -20,9 +21,9 @@ while True:
         elif entered_number < hidden_number:
             print('Бери больше')
         else:
-            print('Ты угадал!\n')
-            time.sleep(1)
+            print('Ты угадал, ты красавчик!')
             break
+        time.sleep(1)
 
         entered_number = int(input('Попытка #2 <ввод тут>: '))
         if entered_number > hidden_number:
@@ -30,20 +31,21 @@ while True:
         elif entered_number < hidden_number:
             print('Бери больше')
         else:
-            print('Ты угадал!\n')
-            time.sleep(1)
+            print('Ты угадал, ты красавчик!')
             break
+        time.sleep(1)
 
         entered_number = int(input('Попытка #3 <ввод тут>: '))
         if entered_number == hidden_number:
-            print('Ты угадал\n')
+            print('Ты угадал, ты красавчик!')
+            break
         else:
             print('Ты не угадал! Не отчаивайся, повезёт в следующий раз!')
-            time.sleep(1)
             break
 
     elif question == 'n':
         print('Значит сыграем в другой раз')
         break
-    time.sleep(1.5)
+    time.sleep(1)
+time.sleep(1)
 print('До новых встреч!')
