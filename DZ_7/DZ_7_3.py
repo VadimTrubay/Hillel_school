52a26733fc12e7fe18b6fbfd72d05dd742b152dc
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-23 15:29:11 +0300

    Initial commit

--- DZ_7/DZ_7_3.py
+++ DZ_7/DZ_7_3.py
@@ -9,37 +9,36 @@ print('Игра угадай число за три попытки.')
 hidden_number = random.randint(1, 10)
 
 while True:
-    question = input('Сыграем? y/n >: \n')
-    if question == 'y':
+    question = input('Сыграем? yes/no >: ')
+    if question == 'yes':
 
-        entered_number = int(input('Попытка #1 <ввод тут>: '))
-        if entered_number > hidden_number:
+        entered_number_1 = int(input('Попытка #1 <ввод тут>: '))
+        if entered_number_1 > hidden_number:
             print('Бери меньше')
-        elif entered_number < hidden_number:
+        elif entered_number_1 < hidden_number:
             print('Бери больше')
         else:
             print('Ты угадал, ты красавчик!')
             break
 
-        entered_number = int(input('Попытка #2 <ввод тут>: '))
-        if entered_number > hidden_number:
+        entered_number_2 = int(input('Попытка #2 <ввод тут>: '))
+        if entered_number_2 > hidden_number:
             print('Бери меньше')
-        elif entered_number < hidden_number:
+        elif entered_number_2 < hidden_number:
             print('Бери больше')
         else:
             print('Ты угадал, ты красавчик!')
             break
 
-        entered_number = int(input('Попытка #3 <ввод тут>: '))
-        if entered_number == hidden_number:
+        entered_number_3 = int(input('Попытка #3 <ввод тут>: '))
+        if entered_number_3 == hidden_number:
             print('Ты угадал, ты красавчик!')
             break
         else:
             print('Ты не угадал! Не отчаивайся, повезёт в следующий раз!')
             break
 
-    elif question == 'n':
-        print('Значит сыграем в другой раз')
+    elif question == 'no':
+        print('Значит, сыграем в другой раз')
         break
-
 print('До новых встреч!')
