b6cb3c4d98e20dda9c8691f6ac64c61deedaa290
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-24 12:10:13 +0300

    Initial commit

--- DZ_1/temp.py
+++ DZ_1/temp.py
@@ -1,15 +1,1 @@
-S = r'\n\n\\'[:-1]
-print(S)
-S = r'\n\n' + '\\'
-print(S)
-S = '\\n\\n'
-print(S)
 
-# alphabet = alphabet_eng:
-#     if language == 'eng':
-#         else alphabet_ru
-#
-#
-# for n in range(1, 4):
-#     entered_number = int(input(f'Попытка #{n}>: '))
-#     n += 1
