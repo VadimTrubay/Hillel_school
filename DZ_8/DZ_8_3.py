d154f5b9115e0e67f83a3288758fe634d4cebd29
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-25 07:43:55 +0300

    Initial commit

--- DZ_8/DZ_8_3.py
+++ DZ_8/DZ_8_3.py
@@ -6,7 +6,7 @@
 # 4 - у пользователя есть цифры, буквы нижнего и верхнего регистра
 # 5 - у пользователя есть цифры, буквы нижнего и верхнего регистра, спец. символы и длинна пароля больше 8 символов
 
-password = input('Введите пароль для проверки на сложность >: ')
+password = input('Enter password to verify >: ')
 space = 0
 lower = 0
 upper = 0
@@ -14,7 +14,7 @@ digit = 0
 lenght = 0
 
 if password == 'qwerty' or password == 'admin' or password == '':  # 1 сложность
-    print('Сложность пароля равна 1')
+    print('Password complexity is 1')
     exit()
 
 for symbol in password:  # 5 сложность
@@ -31,7 +31,7 @@ for symbol in password:  # 5 сложность
 if upper != 0 and lower != 0 \
         and digit != 0 and space != 0 \
         and lenght > 8:
-    print('Сложность пароля равна 5')
+    print('Password complexity is 5')
     exit()
 
 for symbol in password:  # 4 сложность
@@ -42,7 +42,7 @@ for symbol in password:  # 4 сложность
     if symbol.isupper():
         digit += 1
 if lower != 0 and digit != 0 and upper != 0:
-    print('Сложность пароля равна 4')
+    print('Password complexity is 4')
     exit()
 
 for symbol in password:  # 3 сложность
@@ -51,13 +51,9 @@ for symbol in password:  # 3 сложность
     if symbol.isdigit():
         digit += 1
 if lower != 0 and digit != 0:
-    print('Сложность пароля равна 3')
+    print('Password complexity is 3')
     exit()
 
 if password.islower():  # 2 сложность
-    print('Сложность пароля равна 2')
-    exit()
-
-if password == 'qwerty' or password == 'admin' or password == '':  # 1 сложность
-    print('Сложность пароля равна 1')
+    print('Password complexity is 2')
     exit()
