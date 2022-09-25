0ebd9215cd1187d659597cb6e936e7e63bdbf441
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-25 10:31:34 +0300

    Initial commit

--- DZ_8/DZ_8_DOP_3.py
+++ DZ_8/DZ_8_DOP_3.py
@@ -1,3 +1,13 @@
 # Задание 3:
 # Пользователь вводит строку. Если она начинается на 'abc',
 # то заменить их на 'www', иначе добавить в конец строки 'zzz'.
+
+text = input('Enter text >: ')
+
+end_str = 'zzz'
+
+if text.startswith('abc'):
+    print(text.replace('abc', 'www'))
+else:
+    text += end_str
+    print(text)
