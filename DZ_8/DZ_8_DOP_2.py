9502473cc3f73e9648c57e020fbb28900b7d4ae2
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 10:04:22 +0300

    Initial commit

--- DZ_8/DZ_8_DOP_2.py
+++ DZ_8/DZ_8_DOP_2.py
@@ -2,14 +2,12 @@
 # Пользователь вводит текст и слово которое нужно найти,
 # если это слово есть в тексте, вывести 'YES', иначе 'NO'
 
-text = input('Enter text >: ')
+enter_text = input('Enter text >: ')
 wd = input('Enter a search term >: ')
 
-new_text = text.split()
-
+new_text = enter_text.split()
 if wd not in new_text:
     print('NO')
     exit(4)
-
 if new_text.index(wd) != -1:
     print('YES')
