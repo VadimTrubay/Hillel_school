f3e2782eca07391c0b620b3392047bd27450d393
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 10:07:13 +0300

    Initial commit

--- DZ_8/DZ_8_DOP_4.py
+++ DZ_8/DZ_8_DOP_4.py
@@ -1,10 +1,10 @@
 # Пользователь вводит текст, удалить в
 # тексте все цифры и вывести строку пользователю.
 
-string = input('Enter test string >: ')
+test_string = input('Enter test string >: ')
 new_string = ''
 
-for symbol in string:
+for symbol in test_string:
     if symbol not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'):
         new_string += symbol
 print(new_string)
