4a7c69ab586cb47f4a8418b7d68564c653eef394
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 10:09:10 +0300

    Initial commit

--- DZ_8/DZ_8_DOP_5.py
+++ DZ_8/DZ_8_DOP_5.py
@@ -2,11 +2,11 @@
 # Написать валидатор для почты. Пользователь вводит почту, а программа должна проверить,
 # что в почте есть символ '@' и '.', и если это так, то вывести "YES", иначе "NO"
 
-string = input('Enter test string >: ')
+test_string = input('Enter test string >: ')
 
-if '@' in string:
+if '@' in test_string:
     print('YES')
-elif '.' in string:
+elif '.' in test_string:
     print('YES')
 else:
     print('NO')
