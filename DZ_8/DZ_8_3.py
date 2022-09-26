2a3226d9b19d1b3b580999062e3f01b07f25ba64
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 22:29:36 +0300

    Initial commit

--- DZ_8/DZ_8_3.py
+++ DZ_8/DZ_8_3.py
@@ -41,7 +41,10 @@ for symbol in enter_password:  # 4 сложность
         symbol_digit += 1
     if symbol.isupper():
         symbol_upper += 1
-if symbol_lower != 0 and symbol_digit != 0 and symbol_upper != 0:
+    if len(enter_password):
+        len_password += 1
+if symbol_lower != 0 and symbol_digit != 0 \
+        and symbol_upper != 0 and len_password != 0:
     print('Password complexity is 4')
     exit()
 
