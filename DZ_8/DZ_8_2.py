97333be4692e71fcc063bbcb6c0303d0918b0af0
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 10:15:30 +0300

    Initial commit

--- DZ_8/DZ_8_2.py
+++ DZ_8/DZ_8_2.py
@@ -1,15 +1,15 @@
 # Напишите программу где пользователь вводит число - count,
 # а программа выводит count чисел фибоначчи.
 
-number = int(input('Enter number >: '))
+count = int(input('Enter number >: '))
 fib_num_1 = 0
 fib_num_2 = 1
 
-if number == 0:
+if count == 0:
     print(0)
     exit(4)
 else:
     print(fib_num_1)
-for i in range(number + 1):
+for i in range(count + 1):
     fib_num_1, fib_num_2 = fib_num_2, fib_num_1 + fib_num_2
     print(fib_num_1)
