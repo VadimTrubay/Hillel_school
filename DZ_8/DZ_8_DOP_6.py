54f8330c71089a10f18171a5a9f20b1fa8a1424e
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 10:10:29 +0300

    Initial commit

--- DZ_8/DZ_8_DOP_6.py
+++ DZ_8/DZ_8_DOP_6.py
@@ -2,10 +2,10 @@
 # Пользователь вводит строку в котором есть буквы и цифры,
 # необходимо из этой строки спарсить целое число.
 
-string = input('Enter test string >: ')
+test_string = input('Enter test string >: ')
 new_string = ''
 
-for symbol in string:
+for symbol in test_string:
     if symbol in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'):
         new_string += symbol
 print(new_string)
