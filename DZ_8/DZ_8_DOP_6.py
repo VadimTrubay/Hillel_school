1c20964d65d5ff141b82399b49365df60be939f6
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 09:15:17 +0300

    Initial commit

---
+++ DZ_8/DZ_8_DOP_6.py
@@ -0,0 +1,11 @@
+# Задание 6:
+# Пользователь вводит строку в котором есть буквы и цифры,
+# необходимо из этой строки спарсить целое число.
+
+string = input('Enter test string >: ')
+new_string = ''
+
+for symbol in string:
+    if symbol in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'):
+        new_string += symbol
+print(new_string)
