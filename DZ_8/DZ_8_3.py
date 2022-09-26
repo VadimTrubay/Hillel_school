0de6ef652ea88d35b4a779ab37a1ce3e301caa53
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-26 09:54:21 +0300

    Initial commit

--- DZ_8/DZ_8_3.py
+++ DZ_8/DZ_8_3.py
@@ -6,54 +6,54 @@
 # 4 - у пользователя есть цифры, буквы нижнего и верхнего регистра
 # 5 - у пользователя есть цифры, буквы нижнего и верхнего регистра, спец. символы и длинна пароля больше 8 символов
 
-password = input('Enter password to verify >: ')
-space = 0
-lower = 0
-upper = 0
-digit = 0
-lenght = 0
+enter_password = input('Enter password to verify >: ')
+symbol_space = 0
+symbol_lower = 0
+symbol_upper = 0
+symbol_digit = 0
+len_password = 0
 
-if password == 'qwerty' or password == 'admin' or password == '':  # 1 сложность
+if enter_password == 'qwerty' or enter_password == 'admin' or enter_password == '':  # 1 сложность
     print('Password complexity is 1')
     exit()
 
-for symbol in password:  # 5 сложность
+for symbol in enter_password:  # 5 сложность
     if symbol.isspace():
-        space += 1
+        symbol_space += 1
     if symbol.islower():
-        lower += 1
+        symbol_lower += 1
     if symbol.isupper():
-        upper += 1
+        symbol_upper+= 1
     if symbol.isdigit():
-        digit += 1
-    if len(password):
-        lenght += 1
-if upper != 0 and lower != 0 \
-        and digit != 0 and space != 0 \
-        and lenght > 8:
+        symbol_digit += 1
+    if len(enter_password):
+        len_password += 1
+if symbol_upper != 0 and symbol_lower != 0 \
+        and symbol_digit != 0 and symbol_space != 0 \
+        and len_password > 8:
     print('Password complexity is 5')
     exit()
 
-for symbol in password:  # 4 сложность
+for symbol in enter_password:  # 4 сложность
     if symbol.islower():
-        lower += 1
+        symbol_lower += 1
     if symbol.isdigit():
-        digit += 1
+        symbol_digit += 1
     if symbol.isupper():
-        digit += 1
-if lower != 0 and digit != 0 and upper != 0:
+        symbol_upper += 1
+if symbol_lower != 0 and symbol_digit != 0 and symbol_upper != 0:
     print('Password complexity is 4')
     exit()
 
-for symbol in password:  # 3 сложность
+for symbol in enter_password:  # 3 сложность
     if symbol.islower():
-        lower += 1
+        symbol_lower += 1
     if symbol.isdigit():
-        digit += 1
-if lower != 0 and digit != 0:
+        symbol_digit += 1
+if symbol_lower != 0 and symbol_digit != 0:
     print('Password complexity is 3')
     exit()
 
-if password.islower():  # 2 сложность
+if enter_password.islower():  # 2 сложность
     print('Password complexity is 2')
     exit()
