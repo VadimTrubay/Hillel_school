f6208d9c95941b5f6b4c527a7140fddab89b42ac
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-28 23:01:01 +0300

    Initial commit

--- DZ_8/DZ_8_3.py
+++ DZ_8/DZ_8_3.py
@@ -9,7 +9,6 @@
 enter_password = input('Enter password to verify >: ')
 punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
 symbol_punctuation = 0
-symbol_space = 0
 symbol_lower = 0
 symbol_upper = 0
 symbol_digit = 0
@@ -31,53 +30,43 @@ for symbol in enter_password:  # 5 сложность
 all_symbols = symbol_punctuation + symbol_lower + symbol_upper + symbol_digit
 if symbol_upper != 0 and symbol_lower != 0 \
         and symbol_digit != 0 and symbol_punctuation != 0 \
-        != 0 and all_symbols >= 8:
-    print(all_symbols)
+        and all_symbols >= 8:
     print('Password complexity is 5')
-    print(all_symbols)
-    print(symbol_punctuation)
-    print(symbol_digit)
-    print(symbol_upper)
-    print(symbol_lower)
     exit()
 
-# for symbol in enter_password:  # 4 сложность
-#     for i in punctuation:
-#         if i == symbol:
-#             symbol_punctuation += 1
-#     if symbol.islower():
-#         symbol_lower += 1
-#     if symbol.isdigit():
-#         symbol_digit += 1
-#     if symbol.isupper():
-#         symbol_upper += 1
-# all_symbols = symbol_punctuation + symbol_space + symbol_lower + symbol_upper + symbol_digit
-# if symbol_lower != 0 and symbol_digit != 0 \
-#         and symbol_upper != 0 and all_symbols < 8:
-#     print(all_symbols)
-#     print('Password complexity is 4')
-#     exit()
-#
-# for symbol in enter_password:  # 3 сложность
-#     if symbol.islower():
-#         symbol_lower += 1
-#     if symbol.isdigit():
-#         symbol_digit += 1
-#     if symbol.isupper():
-#         symbol_upper += 1
-# if symbol_lower != 0 and symbol_digit != 0 or \
-#         symbol_lower != 0 and symbol_upper != 0 or \
-#         symbol_digit != 0 and symbol_upper != 0:
-#     print('Password complexity is 3')
-#     exit()
-#
-# for symbol in enter_password:  # 2 сложность
-#     for i in punctuation:
-#         if i == symbol:
-#             symbol_punctuation += 1
-# if enter_password.islower() or \
-#         enter_password.isupper() or \
-#         enter_password.isdigit() or \
-#         symbol_punctuation != 0:
-#     print('Password complexity is 2')
-#     exit()
+for symbol in enter_password:  # 4 сложность
+    if symbol.islower():
+        symbol_lower += 1
+    if symbol.isdigit():
+        symbol_digit += 1
+    if symbol.isupper():
+        symbol_upper += 1
+all_symbols = symbol_punctuation + symbol_lower + symbol_upper + symbol_digit
+if symbol_lower != 0 and symbol_digit != 0 \
+        and symbol_upper != 0:
+    print('Password complexity is 4')
+    exit()
+
+for symbol in enter_password:  # 3 сложность
+    if symbol.islower():
+        symbol_lower += 1
+    if symbol.isdigit():
+        symbol_digit += 1
+    if symbol.isupper():
+        symbol_upper += 1
+if symbol_lower != 0 and symbol_digit != 0 or \
+        symbol_lower != 0 and symbol_upper != 0 or \
+        symbol_digit != 0 and symbol_upper != 0:
+    print('Password complexity is 3')
+    exit()
+
+for symbol in enter_password:  # 2 сложность
+    for i in punctuation:
+        if i == symbol:
+            symbol_punctuation += 1
+if enter_password.islower() or \
+        enter_password.isupper() or \
+        enter_password.isdigit() or \
+        symbol_punctuation != 0:
+    print('Password complexity is 2')
+    exit()
