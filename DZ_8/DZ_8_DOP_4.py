4370c85277a4cdc17626da1b8b5a0697c39007a8
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-29 23:54:51 +0300

    Initial commit

--- DZ_8/DZ_8_DOP_4.py
+++ DZ_8/DZ_8_DOP_4.py
@@ -3,8 +3,9 @@
 
 test_string = input('Enter test string >: ')
 new_string = ''
+list_number = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
 
 for symbol in test_string:
-    if symbol not in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'):
+    if symbol not in list_number:
         new_string += symbol
 print(new_string)
