3954e5fed7bff5f909cb93b79cdcf3f0005e48ce
VadimTrubay <vadnetvadnet@ukr.net>
2022-09-29 21:50:34 +0300

    Initial commit

---
+++ DZ_9/DZ_9_3.py
@@ -0,0 +1,17 @@
+# Задание 3:
+# Запросить у пользователя 10 чисел и записать их в список A
+# Запросить у пользователя число N
+# Вывести пользователю сколько в списке A повторяется число N
+
+input_number = 1
+A = []
+for i in range(0, 10):
+    A.append(int(input(f'Enter number {input_number} >: ')))
+    input_number += 1
+B = int(input(f'Enter number N >: '))
+amount_N = 0
+for j in A:
+    if j == B:
+        amount_N += 1
+
+print(f'In the list A the number N is repeated {amount_N} times')
