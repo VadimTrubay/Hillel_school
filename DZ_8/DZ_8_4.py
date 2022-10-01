cc903db496a81d0bc6f346500ae24453ab773ffa
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-01 23:15:55 +0300

    Initial commit

--- DZ_8/DZ_8_4.py
+++ DZ_8/DZ_8_4.py
@@ -13,18 +13,11 @@ symbol_len = 0
 password = ''
 
 while True:
-    symbol_len = int(input('Еnter the symbols to generate a password >: '))
+    symbol_len = int(input('Enter the symbols to generate a password >: '))
     if symbol_len < 4:
         print('Error: invalid digit, repeat input', end='\n\n')
 
     elif 4 <= symbol_len <= 8:
         for i in range(symbol_len):
-            password += random.choice(lower_case + upper_case + digits)
-        print(f'Your {symbol_len} digit password is >: {password}')
-        break
-
-    elif symbol_len > 8:
-        for i in range(symbol_len):
             password += random.choice(lower_case + upper_case + digits + punctuation)
         print(f'Your {symbol_len} digit password is >: {password}')
-        break
