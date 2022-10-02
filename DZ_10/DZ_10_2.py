4e4177b371288fd802213a57c66ea640f2a66160
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-02 18:12:09 +0300

    4

--- DZ_10/DZ_10_2.py
+++ DZ_10/DZ_10_2.py
@@ -11,3 +11,18 @@
 # 8 1
 # 7 5
 # 2 1
+
+a = [
+    [1, 6, 8, 5, 4, 0, 3],
+    [5, 7, 8, 9, 4, 2, 1],
+    [6, 0, 7, 8, 1, 2, 5],
+    [5, 7, 2, 7, 5, 2, 1]
+    ]
+
+for x, x_value in enumerate(a):
+    print(x, x_value)
+    for y, y_value in enumerate(x_value):
+        if y % 2 == 0:
+            print(y, y_value)
+
+
