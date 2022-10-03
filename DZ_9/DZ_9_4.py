4019abbb913f7f6ca0cfa74c4e1fd9dacb97ca72
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-03 21:48:39 +0300

    4

--- DZ_9/DZ_9_4.py
+++ DZ_9/DZ_9_4.py
@@ -2,11 +2,10 @@
 # Запросить у пользователя N чисел и записать их в список A
 # Вывести список в обратной последовательности
 
-input_number = 1
 A = []
 B = int(input('Enter number N >: '))
 for i in range(0, B):
-    A.append(int(input(f'Enter number {input_number} >: ')))
-    input_number += 1
+    A.append(int(input(f'Enter number {i + 1} >: ')))
+
 A.reverse()
 print(f'revers A = {A}')
