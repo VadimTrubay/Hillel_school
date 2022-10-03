de34d976da798e36d95212f758b12422016583bf
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-03 22:21:52 +0300

    4

--- DZ_1/temp.py
+++ DZ_1/temp.py
@@ -1,22 +1,13 @@
-# input_number = 1
-# A = []
-# B = int(input(f'Enter number N >: '))
-# for i in range(0, B):
-#     A.append(int(input(f'Enter number {input_number} >: ')))
-#     input_number += 1
-A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-min_value = A[0]
-max_value = 0
-for j in A:
-    if j > max_value:
-        max_value = j
-    if j < min_value:
-        min_value = j
-print(min_value, max_value)
-
-
-    # n = int(input())
-    # if max_number is None or n > max_number:
-    #     max_number = n
-# print(max_number)
+a = [
+    [1, 6, 8, 5, 4, 0, 3],
+    [5, 7, 8, 9, 4, 2, 1],
+    [6, 0, 7, 8, 1, 2, 5],
+    [5, 7, 2, 7, 5, 2, 1]
+    ]
+for x, x_value in enumerate(a):
+    print()
+    for y, y_value in enumerate(x_value):
+        if y % 2 == 0:
+            print(y_value, end=' ')
+        # if y > x:
+        #     print(y, end=' ')
