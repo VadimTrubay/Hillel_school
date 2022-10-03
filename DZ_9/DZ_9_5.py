1e8568e53fda352dcbe6ac2d1a39a5d4871b42a6
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-03 21:49:20 +0300

    4

--- DZ_9/DZ_9_5.py
+++ DZ_9/DZ_9_5.py
@@ -2,13 +2,11 @@
 # Запросить у пользователя 5 чисел и записать их в список A
 # Записать все числа из списка A которые больше 5 в список C
 
-input_number = 1
 A = []
 C = []
 
 for i in range(0, 5):
-    A.append(int(input(f'Enter number {input_number} >: ')))
-    input_number += 1
+    A.append(int(input(f'Enter number {i +1} >: ')))
 for j in A:
     if j > 5:
         C.append(j)
