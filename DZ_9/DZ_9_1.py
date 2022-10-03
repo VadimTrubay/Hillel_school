4081a976d6608c1733db8c9469d9fd721fa33dc3
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-03 21:45:48 +0300

    4

--- DZ_9/DZ_9_1.py
+++ DZ_9/DZ_9_1.py
@@ -1,10 +1,8 @@
 # Задание 1:
 # Запросить у пользователя 5 чисел и записать их в список
 
-input_number = 1
-list_number = []
 
+list_number = []
 for i in range(0, 5):
-    list_number.append(int(input(f'Enter number {input_number} >: ')))
-    input_number += 1
+    list_number.append(int(input(f'Enter number {i + 1} >: ')))
 print(f'Your list = {list_number}')
