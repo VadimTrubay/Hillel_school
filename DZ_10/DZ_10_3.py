70874b76016b3e10a0d7e024f9b23f69a5d94146
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-06 14:43:42 +0300

    4

--- DZ_10/DZ_10_3.py
+++ DZ_10/DZ_10_3.py
@@ -3,7 +3,7 @@
 # Необходимо учитывать то, что есть разные выигрышные варианты и программа должна их распознавать.
 
 print('Is this game X or 0!')
-print('Enter symbols X or 0 according to the scheme\n\n'
+print('Enter symbols X or 0 according to the scheme\n'
       '1 | 2 | 3 \n'
       '---------\n'
       '4 | 5 | 6 \n'
@@ -20,6 +20,7 @@ def my_print():
     print(a[0])
     print(a[1])
     print(a[2])
+    print()
 
 
 for i in range(0, 9):
@@ -28,58 +29,57 @@ for i in range(0, 9):
     if cell_number == 1:
         a[0][0] = value
         my_print()
-    if cell_number == 2:
+    elif cell_number == 2:
         a[0][1] = value
         my_print()
-    if cell_number == 3:
+    elif cell_number == 3:
         a[0][2] = value
         my_print()
-    if cell_number == 4:
+    elif cell_number == 4:
         a[1][0] = value
         my_print()
-    if cell_number == 5:
+    elif cell_number == 5:
         a[1][1] = value
         my_print()
-    if cell_number == 6:
+    elif cell_number == 6:
         a[1][2] = value
         my_print()
-    if cell_number == 7:
+    elif cell_number == 7:
         a[2][0] = value
         my_print()
-    if cell_number == 8:
+    elif cell_number == 8:
         a[2][1] = value
         my_print()
-    if cell_number == 9:
+    elif cell_number == 9:
         a[2][2] = value
         my_print()
 
     zero = a[0][0] == a[0][1] == a[0][2] == '0' or \
-           a[1][0] == a[1][1] == a[1][2] == '0' or \
-           a[2][0] == a[2][1] == a[2][2] == '0' or \
-           a[0][0] == a[1][0] == a[2][0] == '0' or \
-           a[0][1] == a[1][1] == a[2][1] == '0' or \
-           a[0][2] == a[1][2] == a[2][2] == '0' or \
-           a[0][0] == a[1][1] == a[2][2] == '0' or \
-           a[0][2] == a[1][1] == a[2][0] == '0'
-
+        a[1][0] == a[1][1] == a[1][2] == '0' or \
+        a[2][0] == a[2][1] == a[2][2] == '0' or \
+        a[0][0] == a[1][0] == a[2][0] == '0' or \
+        a[0][1] == a[1][1] == a[2][1] == '0' or \
+        a[0][2] == a[1][2] == a[2][2] == '0' or \
+        a[0][0] == a[1][1] == a[2][2] == '0' or \
+        a[0][2] == a[1][1] == a[2][0] == '0'
     if zero:
         print()
         print('VICTORY PLAYER "0"!')
         exit()
 
     xero = a[0][0] == a[0][1] == a[0][2] == 'x' or \
-           a[1][0] == a[1][1] == a[1][2] == 'x' or \
-           a[2][0] == a[2][1] == a[2][2] == 'x' or \
-           a[0][0] == a[1][0] == a[2][0] == 'x' or \
-           a[0][1] == a[1][1] == a[2][1] == 'x' or \
-           a[0][2] == a[1][2] == a[2][2] == 'x' or \
-           a[0][0] == a[1][1] == a[2][2] == 'x' or \
-           a[0][2] == a[1][1] == a[2][0] == 'x'
-
+        a[1][0] == a[1][1] == a[1][2] == 'x' or \
+        a[2][0] == a[2][1] == a[2][2] == 'x' or \
+        a[0][0] == a[1][0] == a[2][0] == 'x' or \
+        a[0][1] == a[1][1] == a[2][1] == 'x' or \
+        a[0][2] == a[1][2] == a[2][2] == 'x' or \
+        a[0][0] == a[1][1] == a[2][2] == 'x' or \
+        a[0][2] == a[1][1] == a[2][0] == 'x'
     if xero:
         print()
         print('VICTORY PLAYER "X"!')
         exit()
+
     if i >= 8:
         print()
         print('DRAW!')
