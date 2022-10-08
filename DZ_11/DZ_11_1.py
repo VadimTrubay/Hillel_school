c115c8476a64f5116ccf5ab72defd4fa913b7681
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-08 21:08:20 +0300

    4

--- DZ_11/DZ_11_1.py
+++ DZ_11/DZ_11_1.py
@@ -1,5 +1,13 @@
-A_debtors_sept = {"vasia", "petia", "kolia"}
-B_debtors_okt = {"oleg", "sasha", "misha"}
-C_all = A_debtors_sept.union(B_debtors_okt)
+# Задание #1:
+# Дано два множества A и B
+# В множестве А находятся имена должников за Сентябрь
+# В множестве B находятся имена должников за Октябрь
+# Найти:
+# * Вывести имена людей которые должны за Сентябрь и Октябрь
+# * Вывести должников за Октябрь у которых нет долга за Сентябрь
 
-print(f'debtors september and oktober = {C_all}')
+A_debtors_sept = {"1", "2", "3", "5", "6", "8"}
+B_debtors_okt = {"2", "3", "6", "9", "10", "11", "12"}
+
+print(f'debtors september and oktober = {A_debtors_sept.union(B_debtors_okt)}')
+print(f'debtors only for october = {B_debtors_okt.difference(A_debtors_sept)}')
