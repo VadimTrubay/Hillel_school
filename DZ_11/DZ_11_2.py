9ac1219af827bcdb3e9771db6efd3bb60a30ae0d
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-13 18:37:11 +0300

    4

--- DZ_11/DZ_11_2.py
+++ DZ_11/DZ_11_2.py
@@ -16,7 +16,7 @@
 # book.txt R W
 # notebook.exe R W X
 # 5
-# execute python.exe
+# read python.exe
 # read book.txt
 # write notebook.exe
 # execute notebook.exe
@@ -28,27 +28,21 @@
 # OK
 # OK
 
-data_files = {}
-i = 0
+action = {
+    'read': 'R',
+    'write': 'W',
+    'execute': 'X',
+}
+
+file = {}
 for i in range(int(input('Enter number of files(digit) >: '))):
     name, *operations = input(f'Enter a string with the file name and allowed operations {i+1}>: ').split()
-    data_files[name] = operations
+    file[name] = set(operations)
 
-j = 0
 for j in range(int(input('Enter number of file requests(digit) >: '))):
     request, name = input(f'Enter request {j+1}>: ').split()
-    if request == 'write':
-        if 'W' in data_files[name]:
-            print('OK')
-        else:
-            print('Access denied')
-    elif request == 'read':
-        if 'R' in data_files[name]:
-            print('OK')
-        else:
-            print('Access denied')
-    elif request == 'execute':
-        if 'X' in data_files[name]:
-            print('OK')
-        else:
-            print('Access denied')
+    if action[request] in file[name]:
+        print('OK')
+    else:
+
+        print('Access denied')
