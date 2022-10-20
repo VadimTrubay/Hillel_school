597ccda903a88287ace19ad641b3ec5a6cba16aa
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-20 23:04:25 +0300

    main

--- final/final_1.py
+++ final/final_1.py
@@ -3,34 +3,42 @@ import random
 A = ['rock', 'scissors', 'paper', 'lizard', 'spock']
 a = [
     {
-     'scissors': 'paper',
-     'paper': 'rock',
-     'rock': 'lizard',
-     'lizard': 'spock',
-     'spock': 'scissors'
+        'scissors': 'paper',
+        'paper': 'rock',
+        'rock': 'lizard',
+        'lizard': 'spock',
+        'spock': 'scissors'
     },
     {
-     'scissors': 'lizard',
-     'lizard': 'paper',
-     'paper': 'spock',
-     'spock': 'rock',
-     'rock': 'scissors'
+        'scissors': 'lizard',
+        'lizard': 'paper',
+        'paper': 'spock',
+        'spock': 'rock',
+        'rock': 'scissors'
     }
 ]
+
 while True:
     player = input('Your choice (rock paper scissors lizard spock)?>: ')
+    print()
     print(f'Player: {player}')
     computer = random.choice(A)
     print(f'Computer: {computer}')
-
     if player == computer:
         print('Draw')
+        print()
         continue
     for x in range(len(a)):
         for i in a[x].items():
             if player == i[0] and computer == i[1]:
                 print("Player WIN!")
+                print()
                 break
             elif computer == i[0] and player == i[1]:
                 print("Computer WIN!")
+                print()
                 break
+    question = input('Repeat (Y/N)?')
+    print()
+    if question != 'Y':
+        exit()
