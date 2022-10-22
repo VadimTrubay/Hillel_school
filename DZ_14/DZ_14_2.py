e4d7848be62f290bc46883744c771778629e0a0e
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-22 15:07:51 +0300

    main

--- DZ_14/DZ_14_2.py
+++ DZ_14/DZ_14_2.py
@@ -1,19 +1,14 @@
 # Дан список словарей, в каждом из словарей есть ключ name и position, он отвечает за
 # расположение элемента в списке.
 # Position всегда должен быть последовательным, например у нас есть список
+# 2. Добавление элемента с любым position, например мы хотим в наш исходный список добавить элемент
+# у которого position = 1, то должны получить:
 # data = [
-# {'name': 'Test 1', 'position': 1},
-# {'name': 'Test 2', 'position': 2},
-# {'name': 'Test 3', 'position': 3},
+# {'name': 'Test 4', 'position': 1}
+# {'name': 'Test 1', 'position': 2},  # +1
+# {'name': 'Test 2', 'position': 3},  # +1
+# {'name': 'Test 3', 'position': 4},  # +1
 # ]
-# И мы хотим удалить элемент у которого position = 2,
-# то на выходе мы должны получить следующий список:
-# data = [
-# {'name': 'Test 1', 'position': 1},
-# {'name': 'Test 3', 'position': 2},  # -1
-# ]
-# Придерживаясь такой логики, необходимо реализовать:
-# 1. Удаление элемента
 # Функционал необходимо реализовать с помощью функций!
 
 data = [
@@ -33,7 +28,7 @@ data = [
 
 
 def delete_item(obj: list, position: int):
-    del data[position]
+    data.append(obj[position])
     position -= 1
     return obj
 
