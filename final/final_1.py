a53d544fc13613f815511996657fbf825e97ba6b
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-22 14:05:22 +0300

    main

--- final/final_1.py
+++ final/final_1.py
@@ -1,4 +1,6 @@
 import random
+import time
+import os
 
 A = ['rock', 'scissors', 'paper', 'lizard', 'spock']
 a = [
@@ -19,15 +21,18 @@ a = [
 ]
 
 while True:
+    os.system('cls')
     player = input('Your choice (rock paper scissors lizard spock)?>: ')
     if player not in A:
         print(f'Invalid input "{player}"')
     else:
         print(f'Player: {player}')
+        time.sleep(1)
         computer = random.choice(A)
         print(f'Computer: {computer}')
+        time.sleep(1)
         if computer == player:
-            print('Draw')
+            print('Draw!')
         for x in range(len(a)):
             for i in a[x].items():
                 if player == i[0] and computer == i[1]:
@@ -36,6 +41,8 @@ while True:
                 elif computer == i[0] and player == i[1]:
                     print('Computer WIN!')
                     break
+    print()
+    time.sleep(1)
     question = input('Repeat (y/n)?>: ')
     if question != 'y':
         exit()
