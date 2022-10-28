7eecba007117ae067a4673648875e258ae04fdfd
VadimTrubay <vadnetvadnet@ukr.net>
2022-10-28 09:59:37 +0300

    main

---
+++ DZ_16/DZ_16.py
@@ -0,0 +1,18 @@
+# Необходимо создать класс Human с атрибутами:
+# name
+# surname
+# age
+# phone
+# address
+# Атрибуты должны заполняться в методе __init__
+# Так-же нужно написать методы:
+# get_info() - который возвращает словарь в котором находится информация о человеке
+# call(phone_number) - который будет выводить "{self.phone} вызывает абонента {phone_number}"
+# Нужно создать 3 обьекта класса Human и вызвать у них метод get_info
+
+class Human(self, name, surname, age, phone, adress):
+    self.name = name
+    self.surname = surname
+    self.age = age
+    self.phone = phone
+    self.adress = adress
